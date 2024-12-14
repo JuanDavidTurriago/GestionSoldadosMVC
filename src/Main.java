fa0efc826062391492650890aa7859a8baf54334
JuanDavidTurriago <juan.turriago@correounivalle.edu.co>
2024-12-13 21:33:18 -0500

    Implementacion interfaz grafica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import vista.VentanaPrincipal;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
     }
 }
